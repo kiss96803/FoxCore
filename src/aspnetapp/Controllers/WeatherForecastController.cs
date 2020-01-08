ec1dd877f109362a4c5f18b6a08df810ad5952c5
Ian Ye <lifeiye@thoughtworks.com>
2020-01-09 00:11:37 +0800

    Update WeatherForecastController.cs

--- src/aspnetapp/Controllers/WeatherForecastController.cs
+++ src/aspnetapp/Controllers/WeatherForecastController.cs
@@ -17,7 +17,7 @@ namespace FoxCore.Controllers
         [HttpGet]
         public string  Get()
         {
-            return "Happy New Year 2020！";
+            return "Happy New Year 2020 From Fox Team！";
         }
     }
 }
